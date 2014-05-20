867917a62614e252cb3520a73a8b908f0ad2a0a1
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-05-20 23:19:44 +0400

    Add main header file

---
+++ Source/sober/sober.hpp
@@ -0,0 +1,56 @@
+#ifndef SOBER_SOBER_HPP_
+#define SOBER_SOBER_HPP_
+
+// Copyright (c) 2014, Ruslan Baratov
+// All rights reserved.
+
+#include <sober/Exit.hpp>
+#include <sober/log/Logger.hpp>
+#include <sober/log/Severity.hpp>
+#include <sober/log/attributes.hpp>
+#include <sober/network/Engine.hpp>
+#include <sober/network/Engine.ipp>
+#include <sober/network/http/ConnectManager.hpp>
+#include <sober/network/http/ConnectManager.ipp>
+#include <sober/network/http/Request.hpp>
+#include <sober/network/http/Request.ipp>
+#include <sober/network/http/Response.hpp>
+#include <sober/network/http/Response.ipp>
+#include <sober/network/http/Sink.hpp>
+#include <sober/network/http/Statistic.hpp>
+#include <sober/network/http/Stream.hpp>
+#include <sober/network/http/delegate/Interface.hpp>
+#include <sober/network/http/delegate/Reconnect.hpp>
+#include <sober/network/http/delegate/Retry.hpp>
+#include <sober/network/http/delegate/Timeout.hpp>
+#include <sober/network/http/request/Method.hpp>
+#include <sober/network/http/response/attribute/ChunkSize.hpp>
+#include <sober/network/http/response/attribute/ContentLength.hpp>
+#include <sober/network/http/response/attribute/Header.hpp>
+#include <sober/network/http/response/attribute/ReasonPhrase.hpp>
+#include <sober/network/http/response/attribute/StatusCode.hpp>
+#include <sober/network/http/response/attribute/StatusLine.hpp>
+#include <sober/network/http/response/attribute/TEXT.hpp>
+#include <sober/network/http/response/attribute/TransferEncoding.hpp>
+#include <sober/network/http/response/grammar/CR.hpp>
+#include <sober/network/http/response/grammar/CRLF.hpp>
+#include <sober/network/http/response/grammar/CTL.hpp>
+#include <sober/network/http/response/grammar/ChunkSize.hpp>
+#include <sober/network/http/response/grammar/ContentLength.hpp>
+#include <sober/network/http/response/grammar/HT.hpp>
+#include <sober/network/http/response/grammar/HTTPVersion.hpp>
+#include <sober/network/http/response/grammar/Header.hpp>
+#include <sober/network/http/response/grammar/LF.hpp>
+#include <sober/network/http/response/grammar/LWS.hpp>
+#include <sober/network/http/response/grammar/ReasonPhrase.hpp>
+#include <sober/network/http/response/grammar/SP.hpp>
+#include <sober/network/http/response/grammar/StatusCode.hpp>
+#include <sober/network/http/response/grammar/StatusLine.hpp>
+#include <sober/network/http/response/grammar/TEXT.hpp>
+#include <sober/network/http/response/grammar/TransferEncoding.hpp>
+#include <sober/network/http/response/grammar/common.hpp>
+#include <sober/network/http/sink/String.hpp>
+#include <sober/utils/Test.hpp>
+#include <sober/utils/run_duration.hpp>
+
+#endif // SOBER_SOBER_HPP_
