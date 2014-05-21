36d78190a8fc47d7cab878eaecf2c61bfb7f612f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-05-21 14:08:03 +0400

    Update build script

--- build-libcxx.sh
+++ build-libcxx.sh
@@ -6,7 +6,9 @@ rm -rf _builds/libcxx
 
 cmake -H. -B_builds/libcxx -DHUNTER_STATUS_DEBUG=ON \
     -DCMAKE_TOOLCHAIN_FILE=${POLLY_ROOT}/libcxx.cmake \
-    -DSOBER_RUN_LONG_TESTS=ON \
+    -DSOBER_BUILD_EXAMPLES=ON \
+    -DSOBER_BUILD_LONG_TESTS=ON \
+    -DSOBER_BUILD_TESTS=ON \
     -GXcode
 
 open _builds/libcxx/Sober.xcodeproj
