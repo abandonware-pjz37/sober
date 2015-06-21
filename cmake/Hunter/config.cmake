3d0ed857178b0b379c867cb7e45dae54ebd88ade
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-21 16:33:10 +0200

    Use Boost 1.55

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,1 +1,3 @@
-hunter_config(Boost VERSION 1.57.0) # 1.58.0 issue https://svn.boost.org/trac/boost/ticket/11410
+# 1.58.0 issue https://svn.boost.org/trac/boost/ticket/11410
+# 1.56.0-1.57.0 Doesn't work on Linux Ubuntu gcc (TODO Why?)
+hunter_config(Boost VERSION 1.55.0-patched-3)
